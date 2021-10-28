418741d28f77dc35bce3962c8aa7a95aa5fe6297
Nikita Buzadji <91666473+LIMVX@users.noreply.github.com>
2021-10-28 14:22:12 +0000

    исправил

--- 1-3.cpp
+++ 1-3.cpp
@@ -1,18 +1,18 @@
 #define _USE_MATH_DEFINES
 
-#include<iostream>
 #include<cmath>
+#include<iostream>
 
 using namespace std;
 
 /**
-* \brief Математическая функция, рассчитывающая значение R
-* \param const double R1 - константа, имеющая тип данных с плавающей точкой двойной точности
-* \param const double R2 - константа, имеющая тип данных с плавающей точкой двойной точности
-* \param const double R3 - константа, имеющая тип данных с плавающей точкой двойной точности
-* \return Возвращает значение R
+* \brief Математическая функция, рассчитывающая значение resistance - общее сопротивление при параллельном соединении
+* \param const double resistance1 - сопротивление 1
+* \param const double resistance2 - сопротивление 2
+* \param const double resistance3 - сопротивление 3
+* \return Возвращает значение R - общее сопротивление при параллельном соединении
 **/
-double getR(const double R1, const double R2, const double R3);
+double getresistance(const double resistance1, const double resistance2, const double resistance3);
 
 /**
  * \brief Точка входа в программу.
@@ -21,13 +21,13 @@ double getR(const double R1, const double R2, const double R3);
 int main()
 {
   cout<<" Введите три числа и нажмите Enter: \n ";
-  double R1, R2, R3;
-  cin >> R1 >> R2 >> R3;
-  const auto R=getR(R1, R2, R3);
-  cout<<" R1= "<< R1 <<" R2= "<< R2 <<" R3= "<< R3 <<" R= "<< R;
+  double resistance1, resistance2, resistance3;
+  cin >> resistance1 >> resistance2 >> resistance3;
+  const auto resistance = getresistance(resistance1, resistance2, resistance3);
+  cout<<" resistance1= "<< resistance1 <<" resistance2= "<< resistance2 <<" resistance3= "<< resistance3 <<" resistance= "<< resistance;
 }
 
-double getR(const double R1, const double R2, const double R3)
+double getresistance(const double resistance1, const double resistance2, const double resistance3)
 {
-  return 1.0/(1.0/R1 + 1.0/R2 + 1.0/R3);
+  return 1.0/(1.0/resistance1 + 1.0/resistance2 + 1.0/resistance3);
 }
